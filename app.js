93987635bb138286dd73bd014cf956db9ed1e28c
arianiapalmer <arianiapalmer@gmail.com>
2022-06-21 18:47:50 -0400

    lab complete

--- app.js
+++ app.js
@@ -1,34 +1,112 @@
+'use strict'
+let img1 = document.getElementById('img1');
+let img2 = document.getElementById('img2');
+let img3 = document.getElementById('img3');
+let randomObject1, randomObject2, randomObject3;
+
+let imgSection = document.getElementById('imgSection')
+const maxRounds = 5;
+let userClicks = 0;
+let resultsButton = document.getElementById('resultsButton');
+let resultsDisplay = document.getElementById('resultsDisplay');
 
 let images = [
-    'img/bag.jpg',
-    'img/banana.jpg',
-    'img/bathroom.jpg',
-    'img/boots.jpg',
-    'img/breakfast.jpg',
-    'img/bubblegum.jpg',
-    'img/chair.jpg',
-    'img/cthulhu.jpg',
-    'img/dog-duck.jpg',
-    'img/dragon.jpg',
-    'img/pen.jpg',
-    'img/pet-sweep.jpg',
-    'img/tauntaun.jpg',
-    'img/unicorn.jpg',
-    'img/water-can.jpg',
-    'img/wine-glass.jpg',
-
+    'bag',
+    'banana',
+    'bathroom',
+    'boots',
+    'breakfast',
+    'bubblegum',
+    'chair',
+    'cthulhu',
+    'dog-duck',
+    'dragon',
+    'pen',
+    'pet-sweep',
+    'scissors',
+    'shark',
+    'tauntaun',
+    'unicorn',
+    'water-can',
+    'wine-glass',
 ]
-var Product = function (name, path, timeShown) {
+let allProducts = [];
+
+let Product = function (name, extension = '.jpg') {
     this.name = name;
-    this.path = path;
-    this.timeShown = timeShown;
+    this.path = `img/${name}${extension}`;
+    this.timeShown = 0;
+    this.timesClicked = 0;
+    allProducts.push(this);
 }
-Product.prototype.generateRandomImg(Product){//how to generate random new product to keep track of times shown. do i nest them in an array?
-    for( let i = 0; i < img.length; i++);
-    let timeShown = 0;
 
+for(let i = 0; i < images.length; i++){
+    new Product(images[i]);
+}
+new Product('sweep', '.png');
 
+let getRandomIndex = function (){
+    return Math.floor(Math.random()* allProducts.length);
+}
+let genRandomImg = function (){
+    let randomImg1, randomImg2, randomImg3;
+    randomImg1 = getRandomIndex();
+    randomImg2 = getRandomIndex();
+    randomImg3 = getRandomIndex();
+
+    while (randomImg1 === randomImg2) {
+        randomImg2 = getRandomIndex();
+    }
+    while (randomImg2 === randomImg3 || randomImg1 === randomImg3){
+        randomImg3 = getRandomIndex();
+    }
+
+    randomObject1 = allProducts[randomImg1];
+    randomObject2 = allProducts[randomImg2];
+    randomObject3 = allProducts[randomImg3];
+
+    img1.src = randomObject1.path;
+    img1.id = randomObject1.name;
+    img2.src = randomObject2.path;
+    img2.id = randomObject2.name;
+    img3.src = randomObject3.path;
+    img3.id = randomObject3.name;
+
+    randomObject1.timeShown += 1;
+    randomObject2.timeShown += 1;
+    randomObject3.timeShown += 1;
+}
+genRandomImg();
+
+function handleVote(event){
+    let name = event.target.id;
+    if (name === randomObject1.name){
+        randomObject1.timesClicked+= 1;
+    } else if (name === randomObject2.name){
+        randomObject2.timesClicked+= 1;
+    } else if (name === randomObject3.name){
+        randomObject3.timesClicked+= 1;
+    }
+    userClicks++
+    if(userClicks === maxRounds){
+        imgSection.removeEventListener('click', handleVote);
+        imgSection.hidden = true;
+        resultsButton.hidden = false;
+
+    }
+    genRandomImg();
+}
+function handleResults(event){
+    let resultsList = document.createElement('ul');
+    resultsDisplay.appendChild(resultsList);
+    for (let i = 0; i < allProducts.length; i++){
+        let resultItem = document.createElement('li');
+        resultItem.textContent = `${allProducts[i].name} had ${allProducts[i].timesClicked} votes, and was seen ${allProducts[i].timeShown} times.`
+        resultsList.appendChild(resultItem);
+    }
 
 }
-var bag = new Product(bag, img/bag.jpg, );
 
+
+imgSection.addEventListener('click', handleVote)
+resultsButton.addEventListener('click', handleResults)
