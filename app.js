3f25e759760a5481bc7fd5c8fd83eb9b5f86efc8
arianiapalmer <arianiapalmer@gmail.com>
2022-06-21 14:32:51 -0400

    added images array

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
+
+let images = [
+    'img/bag.jpg',
+    'img/banana.jpg',
+    'img/bathroom.jpg',
+    'img/boots.jpg',
+    'img/breakfast.jpg',
+    'img/bubblegum.jpg',
+    'img/chair.jpg',
+    'img/cthulhu.jpg',
+    'img/dog-duck.jpg',
+    'img/dragon.jpg',
+    'img/pen.jpg',
+    'img/pet-sweep.jpg',
+    'img/tauntaun.jpg',
+    'img/unicorn.jpg',
+    'img/water-can.jpg',
+    'img/wine-glass.jpg',
+
+]
 var Product = function (name, path, timeShown) {
     this.name = name;
     this.path = path;
     this.timeShown = timeShown;
 }
 Product.prototype.generateRandomImg(Product){//how to generate random new product to keep track of times shown. do i nest them in an array?
-    (for i = 0; i < img.length; i++);
+    for( let i = 0; i < img.length; i++);
     let timeShown = 0;
 
 
