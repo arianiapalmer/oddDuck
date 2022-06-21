4dfdf50047ea307b653930dd9f958fe41999c83f
arianiapalmer <arianiapalmer@gmail.com>
2022-06-21 10:52:48 -0400

    added some html

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-var product = function (name, path, timeShown) {
+var Product = function (name, path, timeShown) {
     this.name = name;
     this.path = path;
     this.timeShown = timeShown;
 }
+Product.prototype.generateRandomImg(Product){//how to generate random new product to keep track of times shown. do i nest them in an array?
+    (for i = 0; i < img.length; i++);
+    let timeShown = 0;
 
-var bag = new product(bag, img/bag.jpg, )
 
-product.prototype.generateRandom(img){
 
 }
+var bag = new Product(bag, img/bag.jpg, );
+
