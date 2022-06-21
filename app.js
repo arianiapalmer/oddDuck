3a3e6dbcdfba1648aabde468ddc3bda5162a8cdd
arianiapalmer <arianiapalmer@gmail.com>
2022-06-21 00:04:54 -0400

    added constructor for products and began prototype for randome images

---
+++ app.js
@@ -0,0 +1,11 @@
+var product = function (name, path, timeShown) {
+    this.name = name;
+    this.path = path;
+    this.timeShown = timeShown;
+}
+
+var bag = new product(bag, img/bag.jpg, )
+
+product.prototype.generateRandom(img){
+
+}
