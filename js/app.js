87414d5f1204ec141c6fdf2f312440a2d3585c33
arianiapalmer <arianiapalmer@gmail.com>
2022-07-07 22:17:58 -0400

    chart is rendering when event see results is fired

--- js/app.js
+++ js/app.js
@@ -5,11 +5,12 @@ let img3 = document.getElementById('img3');
 let randomObject1, randomObject2, randomObject3;
 
 let imgSection = document.getElementById('imgSection')
-const maxRounds = 25;
+const maxRounds = 5;
 let userClicks = 0;
 let resultsButton = document.getElementById('resultsButton');
 let resultsDisplay = document.getElementById('resultsDisplay');
 let totalVotes = [];
+let voteChart = null;
 
 let images = [
     'bag',
@@ -81,24 +82,27 @@ genRandomImg();
 
 function handleVote(event){
     let name = event.target.id;
+    console.log(name);
     if (name === randomObject1.name){
         randomObject1.timesClicked+= 1;
-        timesClicked.push(totalVotes);
+        // totalVotes.push(randomObject1.timesClicked);
+        totalVotes.push(`${randomObject1.name} ${randomObject1.timesClicked}`);
     } else if (name === randomObject2.name){
         randomObject2.timesClicked+= 1;
+        // totalVotes.push(randomObject2.timesClicked);
+        totalVotes.push(`${randomObject2.name} ${randomObject2.timesClicked}`);
     } else if (name === randomObject3.name){
         randomObject3.timesClicked+= 1;
+        // totalVotes.push(randomObject3.timesClicked);
+        totalVotes.push(`${randomObject3.name} ${randomObject3.timesClicked}`);
     }
-    
     userClicks++
     if(userClicks === maxRounds){
         imgSection.removeEventListener('click', handleVote);
         imgSection.hidden = true;
         resultsButton.hidden = false;
-
     }
     genRandomImg();
-    allProducts[i].this.timesClicked.push(totalVotes);
     
 }
 function handleResults(event){
@@ -109,11 +113,58 @@ function handleResults(event){
         resultItem.textContent = `${allProducts[i].name} had ${allProducts[i].timesClicked} votes, and was seen ${allProducts[i].timeShown} times.`
         resultsList.appendChild(resultItem);
     }
-
+    displayChart();
 }
-
 
 imgSection.addEventListener('click', handleVote)
 resultsButton.addEventListener('click', handleResults)
+
+function displayChart(){
+    let chartImgs = [];
+    let chartData = [];
+    for (let i=0; i<allProducts.length; i++){
+        chartImgs[i] = allProducts[i].name;
+        chartData[i] = allProducts[i].timesClicked;
+    }
+    let chartEl =  document.getElementById("myChart").getContext('2d');
+    
+    voteChart = new Chart(chartEl, {
+        type: 'pie',
+        data: {
+            labels: chartImgs,
+            datasets: [{
+                label: 'Number of Votes',
+                data: chartData,
+                backgroundColor: [
+                    'orange',
+                    'green',
+                    'blue',
+                    'red',
+                    'pink',
+                    'yellow',
+                ],
+                hoverOffset: 4,
+
+                borderColor: [
+                    'orange',
+                    'green',
+                    'blue',
+                    'red',
+                    'pink',
+                    'yellow' 
+                ],
+                
+            }]
+        },
+        options: {
+            hover: true,
+            title: {
+                display: true,
+                text: 'Total of Votes Per Product',
+            }
+        }
+    });
+    
+}
 
 
